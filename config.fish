8b2508e49004b7c1b37479a693f8d9b172549648
kamui <kamui@kamui-m1.local>
2021-01-12 07:29:57 +0900

    mod

--- config.fish
+++ config.fish
@@ -1,5 +1,6 @@
 
-#export LSCOLORS=gxfxcxdxbxegedabagacad
+cd ~/dev/ooq/g.prototype/001/assets/
+
 export LSCOLORS=fxfxcxdxexegedabagacad
 #               ^dir    ^exe
 
@@ -8,8 +9,8 @@ set -x PATH /opt/homebrew/bin $PATH
 set -x PATH $HOME/.cargo/bin $PATH
 set -x PATH $HOME/.nodebrew/current/bin $PATH
 set -x RIPGREP_CONFIG_PATH ~/.ripgreprc
-set -x FZF_DEFAULT_COMMAND 'rg --files --hidden --glob "!.git"'
-set -x FZF_CTRL_T_COMMAND 'rg --files --hidden --follow --glob "!.git/*"'
+#set -x FZF_DEFAULT_COMMAND 'rg --files --hidden --glob "!.git"'
+#set -x FZF_CTRL_T_COMMAND  'rg --files --hidden --follow --glob "!.git/*"'
 
 alias ll="la"
 #alias ls="ls -1"
@@ -19,12 +20,11 @@ alias ll="la"
 #alias f="mdfind -onlyin . -name"
 
 # corsor mv char
-#bind \cO backward-char
 bind \cL forward-char
+#bind \cO backward-char
 
 # corsor mv word
 bind \cO backward-word
-#bind \cQ backward-word
 bind \cF forward-word
 
 bind \cK kill-word
@@ -38,6 +38,5 @@ export GREP_COLOR="1;33"
 set fish_color_autosuggestion cyan
 set fish_color_search_match   --background=brmagenta
 #set fish_color_selection      --background=blue
-
 
 
