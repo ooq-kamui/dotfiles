e8b060d465ab667da3ae24f3e33f5412fb1c1fa9
ooq-tiki <yinoue.hyphen@gmail.com>
2023-02-26 04:25:18 +0900

    sakura

---
+++ sh/sakura/line_path_opn.js
@@ -0,0 +1,9 @@
+﻿
+SelectLine(0);
+Left_Sel(0);
+
+var slctd_str = GetSelectedString;
+
+slctd_str = slctd_str.replace(/^\s+|\s+$/g, '');
+
+ExecCommand('explorer ' + '"' + slctd_str + '"', 0);
---
+++ sh/sakura/sakura_dir_opn.js
@@ -0,0 +1,4 @@
+﻿
+var dir_str = 'C:\\Users\\yinou\\AppData\\Roaming\\sakura\\';
+
+ExecCommand('explorer ' + '"' + dir_str + '"', 0);
