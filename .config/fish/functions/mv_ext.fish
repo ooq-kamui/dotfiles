a7a65f5a8fd7176fe5b421c6770a92436671a710
ooq-tiki <yinoue.hyphen@gmail.com>
2023-02-26 22:19:30 +0900

    fish ext ch

---
+++ .config/fish/functions/mv_ext.fish
@@ -0,0 +1,5 @@
+function mv_ext
+  
+  mv $argv[1] ( str_ext_ch $argv[1] $argv[2] )
+end
+
--- .config/fish/functions/str_ext_ch.fish
+++ .config/fish/functions/str_ext_ch.fish
@@ -1,4 +1,4 @@
-function ext_rpl
+function str_ext_ch
   
   string replace -r '\.[^.]+$' .$argv[2] $argv[1]
 end
