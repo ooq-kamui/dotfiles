80ff2be31512142633c21374dffa5b311f925928
ooq-tiki <yinoue.hyphen@gmail.com>
2023-12-02 04:20:51 +0900

    git-bash dl

--- sh/git-bash/dl.sh
+++ sh/git-bash/dl.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+# 
+# curl -Lo ~/sh/dl.sh \
+#   https://raw.githubusercontent.com/ooq-kamui/vimrc/master/sh/git-bash/dl.sh
 
 
 curl -Lo ~/.bashrc \
   https://raw.githubusercontent.com/ooq-kamui/vimrc/master/sh/git-bash/.bashrc
-
 
 curl -Lo ~/.vimrc \
   https://raw.githubusercontent.com/ooq-kamui/vimrc/master/.vimrc
