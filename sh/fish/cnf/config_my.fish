ae430e83ee876432acdbe091ba102e1d5bb70a92
ooq-tiki <ooq.tiki@gmail.com>
2024-07-17 16:40:22 +0900

    fish

--- sh/fish/cnf/config_my.fish
+++ sh/fish/cnf/config_my.fish
@@ -184,4 +184,22 @@ set fish_color_autosuggestion cyan
 set fish_color_search_match   --background=brmagenta
 
 
+# git prompt
+set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showstashstate 'yes'
+set __fish_git_prompt_showuntrackedfiles 'yes'
+set __fish_git_prompt_showupstream 'yes'
+set __fish_git_prompt_color_branch yellow
+set __fish_git_prompt_color_upstream_ahead green
+set __fish_git_prompt_color_upstream_behind red
+ 
+# git prompt  -  status chars
+set __fish_git_prompt_char_dirtystate '⚡'
+set __fish_git_prompt_char_stagedstate '→'
+set __fish_git_prompt_char_untrackedfiles '☡'
+set __fish_git_prompt_char_stashstate '↩'
+set __fish_git_prompt_char_upstream_ahead '+'
+set __fish_git_prompt_char_upstream_behind '-'
+
+
 
--- sh/fish/fnc/fish_prompt.fish
+++ sh/fish/fnc/fish_prompt.fish
@@ -2,7 +2,13 @@ function fish_prompt --description 'Write out the prompt'
   if test -z $WINDOW
     #printf '%s%s ' (set_color purple) (prompt_hostname)
     #printf '%s%s ' (set_color f6f) $USER
-    printf '%s%s ' (set_color f6f) '_'
+    #printf '%s%s ' (set_color f6f) '_'
+    printf '%s%s' (set_color f6f) '_'
+
+    set last_status $status
+    printf '%s ' (__fish_git_prompt)
+    set_color normal
+
   else
     printf '%s%s@%s%s(%s)%s $ ' (set_color purple) (whoami) (prompt_hostname) (set_color white) (echo $WINDOW) (set_color normal)
   end
