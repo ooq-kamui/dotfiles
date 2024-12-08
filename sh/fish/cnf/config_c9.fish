fc35dd29c84b133edbd75090b3109b5343d357b5
ooq-tiki <ooq.tiki@gmail.com>
2024-12-08 18:44:48 +0900

    fish

--- sh/fish/cnf/config_c9.fish
+++ sh/fish/cnf/config_c9.fish
@@ -1,18 +1,5 @@
 
-# alias
-
-#alias clp 'clp_by_tmp'
-#alias clp '~/wrk/cnf/sh/bash/cmd/osc52.sh'
-alias clp 'osc52.sh'
-
-#alias ll "ls -lgohAGD '%Y-%m-%d %H:%M'"
-alias ll 'ls -lgohA --time-style=long-iso --color=auto'
-
-alias vim 'vim -p'
-alias vi  'vim -p'
-
-alias column '/usr/bin/column'
-
+# set
 
 # color
 
@@ -29,6 +16,19 @@ set fish_color_error          grey
 
 #set -x RIPGREP_CONFIG_PATH ~/wrk/cnf/sh/rg/.ripgreprc
 set -x RIPGREP_CONFIG_PATH ~/wrk/cnf/sh/rg/.ripgreprc_c9
+
+
+# alias
+
+alias clp 'osc52.sh'
+
+#alias ll "ls -lgohAGD '%Y-%m-%d %H:%M'"
+alias ll 'ls -lgohA --time-style=long-iso --color=auto'
+
+alias vim 'vim -p'
+alias vi  'vim -p'
+
+alias column '/usr/bin/column'
 
 
 
