ee3a92f899dbe7efec42dfd5352a3c4cad4fb8a4
ooq-tiki <ooq.tiki@gmail.com>
2026-08-26 10:43:40 +0900

    wez

--- app/term/wez-term/cnf/lnx/wezterm-lnx.lua
+++ app/term/wez-term/cnf/lnx/wezterm-lnx.lua
@@ -5,7 +5,7 @@ local act     = wezterm.action
 local config = require('cnf/wezterm')
 
 -- 
--- env : mac
+-- env : lnx
 -- 
 
 config.window_background_opacity    = 0.40 -- = 0.50
@@ -21,7 +21,7 @@ config.use_ime = true
 config.macos_forward_to_ime_modifier_mask = 'SHIFT|CTRL'
 
 -- 
--- env : mac : inch-14
+-- env : lnx : inch-14
 -- 
 
 config.default_prog = {'/usr/bin/fish'}
@@ -33,12 +33,26 @@ config.font_size    =  18
 -- config.initial_cols = 134
 -- config.initial_rows =  37
 
+
 -- gui_startup({
 --   ['HG-245H']  = { x = 43, y = 10, cols = 167, rows = 42 },
 --   ['_default'] = { x = 25, y = 70, cols = 134, rows = 37 },
 -- })
 
 color_scheme__rnd('lnx')
+
+
+-- OpenGL, at Hyper-V, VirtualBox
+config.prefer_egl = true
+config.front_end = "WebGpu" -- or "Software"
+
+-- ssh agent, err off
+-- config.mux_enable_ssh_agent = false
+
+
+-- title bar : on
+-- config.window_decorations = "TITLE | RESIZE"
+
 
 return config
 
--- doc/memo-fdr.md
+++ doc/memo-fdr.md
@@ -6,7 +6,23 @@
 
 setting
 - ofc
-  - same pri ?
+  - same pri
+  - start-up err
+    ```
+    local wezterm = require 'wezterm'
+    local config = wezterm.config_builder()
+    
+    -- 【重要】Hyper-VやVirtualBoxなどの仮想環境でOpenGLを安定させる設定
+    config.prefer_egl = true
+    config.front_end = "WebGpu" -- もし動かない場合は "Software" に変更してください
+    
+    -- SSHエージェントのエラーが気になる場合は無効化（任意）
+    config.mux_enable_ssh_agent = false
+    
+    return config
+    ```
+
+
 - pri
   - win mv
   - start-up err ?
